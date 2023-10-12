9808b860fb4f75b74996d03404fac470cfa29313
eunseo0422 <ukibest@naver.com>
2023-10-12 16:07:53 +0900

    03

--- 06.c
+++ 06.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
  
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func(void)
+int sumTwo(int a, int b)
 {
-	int x;
-	printf("func x is at %p\n", &x);
+	int sum = a + b;
+	return sum;
 }
 
-int main(void)
+int square(int n)
 {
-	int x;
-	printf("main x is at %p\n", &x);
-	func();
+	return n * n;
+}
+
+int get_max(int x, int y)
+{
+	if (x > y)
+		return x;
 	
-return 0;
+	return y;
+}
+
+int main()
+{
+	int a, b;
+	int n;
+	int x, y;
+	
+	a = 3;
+	b = 10;
+	printf("sumTwo result : %i\n", sumTwo(a,b));
+	
+	n = 4;
+	printf("square result : %i\n", square(n));
+	
+	x = 7;
+	y = 8;
+	printf("get_max result : %i\n", get_max(x,y));
 }
